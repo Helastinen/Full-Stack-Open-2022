7a2e5ddc33ffa374ce6b01ce00629b9cc3d9c5ed
Antti <hellsten.antti@gmail.com>
2022-03-27 20:19:03 +0300

    added phonebook exercise 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,16 +1,20 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { PersonForm, Persons, FilterPersons } from './components/Persons'
+import axios from "axios"
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState("")
   const [newNumber, setNewNumber] = useState("")
   const [newFilter, setNewFilter] = useState("")
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/persons")
+      .then((response) => {
+        setPersons(response.data)
+      })
+  }, [])
 
   const handleNewName = (e) => setNewName(e.target.value)
   const handleNewNumber = (e) => setNewNumber(e.target.value)
@@ -21,7 +25,6 @@ const App = () => {
 
   const handleFilter = (e) => {
     setNewFilter(e.target.value)
-    setPersons(filteredPersons)
   }
 
   const addPerson = (e) => {
