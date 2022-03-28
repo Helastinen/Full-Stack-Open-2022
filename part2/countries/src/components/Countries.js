10cfaece2c2827d3b419d7695cba1525eeee0b49
Antti <hellsten.antti@gmail.com>
2022-03-28 11:43:53 +0300

    refactored exercise 2.13

--- part2/countries/src/components/Countries.js
+++ part2/countries/src/components/Countries.js
@@ -1,7 +1,7 @@
 const CountryFilter = ({ newFilter, handleFilter }) => {
   return(
     <>
-    Find countries: <input value={newFilter} onChange={handleFilter}/>
+      Find countries: <input value={newFilter} onChange={handleFilter}/>
     </>
   )
 }
@@ -14,11 +14,8 @@ const Countries = ({ countries, handleShowCountry }) =>  {
     return keys.map(key => <li key={key}>{country.languages[key]}</li>)
   }
 
-  if (countries.length > 10) {
-    return(
-      <div>Too many matches, please be more specific.</div>
-    )
-  } else if (countries.length > 1 && countries.length <= 10) {
+  if (countries.length > 10) return <div>Too many matches, please be more specific.</div>
+  else if (countries.length > 1 && countries.length <= 10) {
     return(
       countries.map(country =>
         <li key={country.name.official}>
@@ -26,24 +23,17 @@ const Countries = ({ countries, handleShowCountry }) =>  {
         </li>
       )
     )
-  } else {
+  } else if (countries.length === 1) {
     return(
       countries.map(country => {
         return (
           <>
             <h3>{country.name.common}</h3>
-            <p>
-              Capital: {country.capital}<br/>
-              Area: {country.area}
-            </p>
-            
+            <p>Capital: {country.capital}<br/>
+              Area: {country.area}</p>
             <b>Languages</b>
-            <ul>
-              {listLanguages(country)}
-            </ul>
-            <p>
-              <img src={country.flags.png} alt="Country flag" width="200px"></img>
-            </p>
+            <ul>{listLanguages(country)}</ul>
+            <p><img src={country.flags.png} alt="Country flag" width="200px"></img></p>
           </>
         )
       })
