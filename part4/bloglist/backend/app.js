8a8dc792bfcb333943aa8c8228dd568510a7fd9c
Antti <hellsten.antti@gmail.com>
2022-04-07 14:04:30 +0300

    add bloglist exer 4.2

--- part4/bloglist/backend/app.js
+++ part4/bloglist/backend/app.js
@@ -1,44 +1,19 @@
-require("dotenv").config()
-const http = require('http')
-const express = require('express')
+
+const express = require("express")
 const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
+const cors = require("cors")
+const mongoose = require("mongoose")
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
+const logger = require("./utils/logger")
+const config = require("./utils/config")
+const bloglistRouter = require("./controllers/bloglist")
 
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = process.env.MONGODB_URI
+//* Create mondoDB connection
+const mongoUrl = config.MONGODB_URI
 mongoose.connect(mongoUrl)
 
 app.use(cors())
 app.use(express.json())
+app.use("/api/blogs", bloglistRouter) // hook up routes with default URL
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+module.exports = app
