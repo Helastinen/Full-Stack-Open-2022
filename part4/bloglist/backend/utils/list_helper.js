d984c5074e3b72eb6157c47fe7d37c4d95db66a5
Antti <hellsten.antti@gmail.com>
2022-04-07 17:31:13 +0300

    add bloglist exer 4.4

--- part4/bloglist/backend/utils/list_helper.js
+++ part4/bloglist/backend/utils/list_helper.js
@@ -2,4 +2,11 @@ const dummy = (blogs) => {
   return 1
 }
 
-module.exports = { dummy }
+const totalLikes = (blogs) => {
+  return blogs.reduce(
+    (likesSum, blog) => likesSum + blog.likes,
+    0
+  )
+}
+
+module.exports = { dummy, totalLikes }
