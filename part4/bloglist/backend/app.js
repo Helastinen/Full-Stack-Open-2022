fe3f9f4f4b1de766f37fd9f75fa17e67abe91751
Antti <hellsten.antti@gmail.com>
2022-04-13 09:23:34 +0300

    add bloglist exer 4.18

--- part4/bloglist/backend/app.js
+++ part4/bloglist/backend/app.js
@@ -1,25 +1,30 @@
-
+//* import express and mongoDB dependencies
 const express = require("express")
 const app = express()
 require("express-async-errors")
 const cors = require("cors")
 const mongoose = require("mongoose")
 
-//* middleware
+//* import middleware
 const logger = require("./utils/logger")
 const config = require("./utils/config")
 
-//* routers
+//* import routers
 const bloglistRouter = require("./controllers/bloglist")
 const usersRouter = require("./controllers/users")
+const loginRouter = require("./controllers/login")
 
 //* Create mondoDB connection
 const mongoUrl = config.MONGODB_URI
 mongoose.connect(mongoUrl)
 
+//* Hook up to app
 app.use(cors())
 app.use(express.json())
-app.use("/api/blogs", bloglistRouter) // hook up blog routes with default URL
-app.use("/api/users", usersRouter) // hook up user routes with default URL
+
+// hook up to router with default URL
+app.use("/api/blogs", bloglistRouter)
+app.use("/api/users", usersRouter)
+app.use("/api/login", loginRouter)
 
 module.exports = app
