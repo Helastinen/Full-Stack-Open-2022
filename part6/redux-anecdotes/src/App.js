3f20a9b7b153337735b2c54152e2eeb8393ddbc0
Antti <hellsten.antti@gmail.com>
2022-04-27 18:18:02 +0300

    add redux-anecdotes exer 6.8.

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -1,30 +1,11 @@
-import { useSelector, useDispatch } from 'react-redux'
-
-import { vote } from "./reducers/anecdoteReducer"
 import AnecdoteForm from './components/AnecdoteForm'
+import AnecdoteList from './components/AnecdoteList'
 
 const App = () => {
-  const anecdotes = useSelector(state => state)
-  const dispatch = useDispatch()
-
-  const addVote = (id) => {
-    dispatch(vote(id))
-  }
-
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.sort((a, b) => b.votes - a.votes).map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => addVote(anecdote.id)}>Vote</button>
-          </div>
-        </div>
-      )}
+      <AnecdoteList />
       <AnecdoteForm />
     </div>
   )
