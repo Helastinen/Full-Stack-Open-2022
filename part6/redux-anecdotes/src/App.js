42ecf1d3635f1980dd5d18c3e4f65a72fabf2fb8
Antti <hellsten.antti@gmail.com>
2022-04-29 15:11:15 +0300

    add redux-anecdotes exer 6.15

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -6,18 +6,13 @@ import Filter from './components/Filter'
 import AnecdoteList from './components/AnecdoteList'
 import AnecdoteForm from './components/AnecdoteForm'
 
-import anecdoteService from './services/anecdotes'
-import { setAnecdotes } from './reducers/anecdoteReducer'
+import { initializeAnecdotes } from './reducers/anecdoteReducer'
 
 const App = () => {
   const dispatch = useDispatch()
 
   useEffect(() => {
-    anecdoteService
-      .getAll()
-      .then(anecdotes => 
-        dispatch(setAnecdotes(anecdotes))
-      )
+    dispatch(initializeAnecdotes())
   }, [dispatch])
 
   return (
--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit"
+import anecdoteService from "../services/anecdotes"
 
 const anecdoteSlice = createSlice({
   name: "anecdote",
@@ -33,6 +34,13 @@ const anecdoteSlice = createSlice({
     }
   }
 })
+
+export const initializeAnecdotes = () => {
+  return async (dispatch) => {
+    const anecdotes = await anecdoteService.getAll()
+    dispatch(setAnecdotes(anecdotes))
+  }
+}
 
 export default anecdoteSlice.reducer
 export const {createAnecdote, vote, appendAnecdote, setAnecdotes} = anecdoteSlice.actions
