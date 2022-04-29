6ae2806c64e40cc6248574788550fa1eacaa113c
Antti <hellsten.antti@gmail.com>
2022-04-29 15:23:16 +0300

    add redux-anecdotes exer 6.16

--- part6/redux-anecdotes/src/components/AnecdoteForm.js
+++ part6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -2,7 +2,6 @@
 import { useDispatch } from 'react-redux'
 import { createAnecdote } from "../reducers/anecdoteReducer"
 import { setNotification, removeNotification } from '../reducers/notificationReducer'
-import anecdoteService from "../services/anecdotes"
 
 const AnecdoteForm = () => {
   const dispatch = useDispatch()
@@ -13,9 +12,7 @@ const AnecdoteForm = () => {
     const content = event.target.anecdote.value
     event.target.anecdote.value = ""
 
-    const newAnecdote = await anecdoteService.create(content)
-    dispatch(createAnecdote(newAnecdote))
-    
+    dispatch(createAnecdote(content))
     dispatch(setNotification(`New anecdote \"${content}\" created`))
     setTimeout(() => 
       dispatch(removeNotification()),
--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -5,10 +5,6 @@ const anecdoteSlice = createSlice({
   name: "anecdote",
   initialState: [],
   reducers: {
-    createAnecdote(state, action) {
-      state.push(action.payload)
-    },
-
     vote(state, action) {
       const id = action.payload
       const anecdotetoChange = state.find(anecdote => anecdote.id === id)
@@ -31,7 +27,7 @@ const anecdoteSlice = createSlice({
 
     setAnecdotes(state, action) {
       return action.payload
-    }
+    },
   }
 })
 
@@ -42,5 +38,12 @@ export const initializeAnecdotes = () => {
   }
 }
 
+export const createAnecdote = (content) => {
+  return async (dispatch) => {
+    const newAnecdote = await anecdoteService.create(content)
+    dispatch(appendAnecdote(newAnecdote))
+  }
+}
+
 export default anecdoteSlice.reducer
-export const {createAnecdote, vote, appendAnecdote, setAnecdotes} = anecdoteSlice.actions
+export const { vote, appendAnecdote, setAnecdotes} = anecdoteSlice.actions
