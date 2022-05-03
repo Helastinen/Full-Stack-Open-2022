bb2cbe2d7459b6a80899dca0de157315b81550f4
Antti <hellsten.antti@gmail.com>
2022-05-03 15:36:10 +0300

    add exer ultimate-hooks 7.8

---
+++ part7/custom-hooks/src/App.js
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+
+const useField = (type) => {
+  const [value, setValue] = useState('')
+  console.log(value)
+  const onChange = (event) => {
+    setValue(event.target.value)
+  }
+
+  return { type, value, onChange}
+}
+
+const App = () => {
+  const name = useField("text")
+  const born = useField("date")
+  const height = useField("number")
+
+  return (
+    <div>
+      <form>
+        name: 
+        <input {...name} /> 
+        <br/> 
+        birthdate:
+        <input {...born} /> 
+        <br /> 
+        height:
+        <input {...height} />
+      </form>
+      <div>
+        {name.value} {born.value} {height.value} 
+      </div>
+    </div>
+  )
+}
+
+export default App
